47520b994ded6d4c88c882edd7fb94e27047ceb5
bigbang1102 <151818283+bigbang1102@users.noreply.github.com>
2023-12-19 08:56:09 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -9,11 +9,15 @@ function validateForm() {
     const username = document.querySelector("input[name='username']");
     if (username.value.length < 3) {
       username.style.backgroundColor = "red";
+      alert('ไม่ถูกต้อง ชื่อผู้ใช้ ห้ามมีตัวเลขนำหน้า \n หรือต้องมีความยาวมากกว่า 3 ตัวอักษร ');
+      changeColor('#username');
       return false;
     }
     const password = document.querySelector("input[name='password']");
     if (password.value.length < 4) {
       password.style.backgroundColor = "red";
+      alert('ไม่ถูกต้อง รหัสผ่านต้องมากกว่า 4 ตัว และต้องมีทั้งตัวเลขและตัวอักษร');
+      changeColor('#password');
       return false;
     }
 
